1ca3ab710518d9f4b15b6bb74af38ff09d9a531b
Goobergreve09 <gregory.greve@yahoo.com>
2023-12-07 01:53:06 -0500

    document formatted

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { promises } from "fs";
 const { writeFile } = promises;
 import { generateSvg } from "./generateSvg.js";
 
-const fileName = 'logo.svg'
+const fileName = "logo.svg";
 
 const enter = "PRESS 'Enter' to skip or save.";
 const enterYellow = `\x1b[33m${enter}\x1b[0m`;
@@ -14,30 +14,30 @@ const enterarrowKeys = `\x1b[33m${arrowKeys}\x1b[0m`;
 const success = "Generatedlogo.svg";
 const successGreen = `\x1b[32m${success}\x1b[0m`;
 
-const shapes = ["circle", "square", "triangle"]
+const shapes = ["circle", "square", "triangle"];
 
 const init = async () => {
   try {
     const answers = await inquirer.prompt([
       {
-        type: 'input',
-        name: 'acronym',
+        type: "input",
+        name: "acronym",
         message: `What 3 letter acronym would you like to use for your logo (ex:DET)? ${enterYellow}\n`,
       },
       {
-        type: 'input',
-        name: 'acronymColor',
+        type: "input",
+        name: "acronymColor",
         message: `\nWhat color would you like the acronym to be? You can type a color or use a hexadecimal. ${enterYellow}\n`,
       },
       {
-        type: 'list',
-        name: 'shape',
+        type: "list",
+        name: "shape",
         message: `\nWhich of the following shapes would you like your logo to be? ${enterarrowKeys}\n`,
         choices: shapes,
       },
       {
-        type: 'input',
-        name: 'shapeColor',
+        type: "input",
+        name: "shapeColor",
         message: `\nWhat color would you like your shape to be? You can type a color or use a hexadecimal. ${enterYellow}\n`,
       },
     ]);
@@ -50,4 +50,3 @@ const init = async () => {
 };
 
 init();
- 
