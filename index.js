c2625bd200e58a19d3fa427de7075e7772c78262
Goobergreve09 <gregory.greve@yahoo.com>
2023-12-08 02:02:52 -0500

    organized file structure and renamed functions

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
 const inquirer = require("inquirer");
 const { writeFile } = require("fs/promises");
-const generateSvg = require("./lib/generateSvg.js")
+const generateShapes = require("./lib/shapes.js");
 
 const fileName = "logo.svg";
 
@@ -12,65 +11,69 @@ const enterarrowKeys = `\x1b[33m${arrowKeys}\x1b[0m`;
 const success = "Generatedlogo.svg";
 const successGreen = `\x1b[32m${success}\x1b[0m`;
 
-const shapes = ["circle", "square", "triangle"];
+const shapes = ["Circle", "Square", "Triangle"];
 
 const promptUser = () => {
   return inquirer.prompt([
-      {
-        type: "input",
-        name: "acronym",
-        message: `What 3 letter acronym would you like to use for your logo (ex:DET)? ${enterYellow}\n`,
-      },
-      {
-        type: "input",
-        name: "acronymColor",
-        message: `\nWhat color would you like the acronym to be? You can type a color or use a hexadecimal. ${enterYellow}\n`,
-      },
-      {
-        type: "list",
-        name: "shape",
-        message: `\nWhich of the following shapes would you like your logo to be? ${enterarrowKeys}\n`,
-        choices: shapes,
-      },
-      {
-        type: "input",
-        name: "shapeColor",
-        message: `\nWhat color would you like your shape to be? You can type a color or use a hexadecimal. ${enterYellow}\n`,
-      },
-    ]);
-  };
+    {
+      type: "input",
+      name: "acronym",
+      message: `What 3 letter acronym would you like to use for your logo (ex:DET)? ${enterYellow}\n`,
+    },
+    {
+      type: "input",
+      name: "acronymColor",
+      message: `\nWhat color would you like the acronym to be? You can type a color or use a hexadecimal. ${enterYellow}\n`,
+    },
+    {
+      type: "list",
+      name: "shape",
+      message: `\nWhich of the following shapes would you like your logo to be? ${enterarrowKeys}\n`,
+      choices: shapes,
+    },
+    {
+      type: "input",
+      name: "shapeColor",
+      message: `\nWhat color would you like your shape to be? You can type a color or use a hexadecimal. ${enterYellow}\n`,
+    },
+  ]);
+};
 
-    const init = () => {
-      promptUser()
-      .then((answers) => {
-        // Adding the length validation for the acronym
-        testLength(answers);
-        testHexidecimal (answers);
-        return answers;
-      })
-        .then((answers) => writeFile(fileName, generateSvg(answers)))
-        .then(() => console.log(`${successGreen}`))
-        .catch((err) => console.error(err));
-    };
+const init = () => {
+  promptUser()
+    .then((answers) => {
+      // Adding the length validation for the acronym
+      testLength(answers);
+      testHexidecimal(answers);
+      return answers;
+    })
+    .then((answers) => writeFile(fileName, generateShapes(answers)))
+    .then(() => console.log(`${successGreen}`))
+    .catch((err) => console.error(err));
+};
 
 init();
 
-function testLength (answers) {
-  if(`${answers.acronym}`.length > 3) {
-    throw new Error('Acronym must not be more than 3 characters long.');
+function testLength(answers) {
+  if (`${answers.acronym}`.length > 3) {
+    throw new Error("Acronym must not be more than 3 characters long.");
   }
+}
+
+function testHexidecimal(answers) {
+  const pattern = /^[A-Za-z0-9#]+$/;
+
+  if (
+    !pattern.test(answers.shapeColor) ||
+    !pattern.test(answers.acronymColor)
+  ) {
+    throw new Error(
+      "A hexidecimal must only contain letters, numbers, and pound symbol (ex. #00013F)"
+    );
   }
+}
 
-  function testHexidecimal (answers) {
-    const pattern = /^[A-Za-z0-9#]+$/;
-
-    if (!pattern.test(answers.shapeColor) || !pattern.test(answers.acronymColor)) {
-      throw new Error('A hexidecimal must only contain letters, numbers, and pound symbol (ex. #00013F)');
-    }
-  }
-
-
-  module.exports = {
-    testLength,
-    testHexidecimal,
-  };
+module.exports = {
+  testLength,
+  testHexidecimal,
+};
